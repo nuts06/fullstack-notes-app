b7f9276023f594b0baee687268c2da66b7dfadc5
Natasha <121818639+nuts06@users.noreply.github.com>
2025-03-23 11:54:43 +0530

    login page created

---
+++ frontend/src/components/input/Password.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6"
+
+const Password = ({password, setPassword}) => {
+    const [isShowPassword, setIsShowPassword] = useState(false)
+    const eyeChange = () =>{
+        setIsShowPassword((prev) => !prev);
+    }
+   
+  return (
+    <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded'>
+        <input 
+            type={isShowPassword ? "text" : "password"} 
+            placeholder='Enter your password' 
+            className='w-full text-sm bg-transparent py-3 mr-3 rounded outline-none' 
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+          />
+          {isShowPassword ? <FaRegEye size={22} className='text-[#2B85FF] cursor-pointer' onClick={eyeChange}/>: (
+            <FaRegEyeSlash size={22} className='text-slate-400 cursor-pointer' onClick={eyeChange}/>
+          )}
+    </div>
+  )
+}
+
+export default Password
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Password from '../../components/input/Password'
+import {Link} from "react-router-dom"
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
   return (
-    <div>Login</div>
+    <div className='flex items-center justify-center mt-28 '>
+      <div className='w-90 rounded bg-white px-7 py-10'>
+        <form action="">
+          <h4 className='text-2xl mb-7 text-center'>Login</h4>
+          <input 
+            type="text" 
+            placeholder='Enter your email' 
+            className='input-box' 
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+          />
+          <Password 
+            password={password}
+            setPassword={setPassword}
+          />
+
+          <button type='submit' className='btn-primary mt-4 h-10 '>Login</button>
+          <p className='text-sm text-center mt-4'>Not Registered Yet? <Link to={"/signup"} className='underline text-blue-600'>Create an account</Link></p>
+        </form>
+      </div>
+    </div>
+    
   )
 }
 
