a12520e311307380a6da96338321f8eb4a797c08
Natasha <121818639+nuts06@users.noreply.github.com>
2025-03-23 11:01:12 +0530

    some global css added

--- frontend/src/components/Cards/Profileinfo.jsx
+++ frontend/src/components/Cards/Profileinfo.jsx
@@ -4,12 +4,12 @@ import { getInitials } from '../../utils/helper'
 const Profileinfo = ({onLogout,Buttontext}) => {
   return (
     <div className='flex items-center gap-3'>
-      <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100'>{getInitials("Natasha Chandrawat ")}</div>
+      <div className='w-12 h-12 cursor-pointer flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100'>{getInitials("Natasha Chandrawat ")}</div>
       <div>
         <p className='text-sm font-medium'>Natasha</p>
       </div>
       <button 
-        className='text-sm bg-red-500 p-1 rounded-md text-white hover:opacity-80'
+        className='text-sm bg-red-500 p-1 rounded-md text-white hover:opacity-80 cursor-pointer'
         onClick={onLogout}
         >{Buttontext}</button>
 
