c03d650823976b9922309a1acf19821e3e89e1a7
Natasha <121818639+nuts06@users.noreply.github.com>
2025-03-25 10:54:14 +0530

    Update login jsx

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -6,10 +6,22 @@ const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState("")
+
+  const handleLogin = async(e) =>{
+    e.preventDeafault()
+
+    if(!validateEmail(email)){
+      setError("Please enter a valid email address")
+      return
+    }
+    if(!password){
+      setError("Please enter the password")
+    }
+  }
   return (
     <div className='flex items-center justify-center mt-28 '>
       <div className='w-90 rounded bg-white px-7 py-10'>
-        <form action="">
+        <form onSubmit={handleLogin}>
           <h4 className='text-2xl mb-7 text-center'>Login</h4>
           <input 
             type="text" 
@@ -24,7 +36,7 @@ const Login = () => {
           />
 
           <button type='submit' className='btn-primary mt-4 h-10 '>Login</button>
-          <p className='text-sm text-center mt-4'>Not Registered Yet? <Link to={"/signup"} className='underline text-blue-600'>Create an account</Link></p>
+          <p className='text-sm text-center mt-4'>Not Registered Yet? {" "}<Link to={"/signup"} className='font-medium underline text-[#2B85FF]'>Create an account</Link></p>
         </form>
       </div>
     </div>
