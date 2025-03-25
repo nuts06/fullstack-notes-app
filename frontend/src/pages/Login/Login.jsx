1b4686e2221aedd5d56628313b90a86027bbe631
Natasha <121818639+nuts06@users.noreply.github.com>
2025-03-25 12:06:15 +0530

    ui fixed for the error

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -6,41 +6,51 @@ import { validateEmail } from '../../utils/helper'
 const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
+  // const [error, setError] = useState("")
+  const [emailError, setEmailError] = useState("")
+  const [passwordError, setPasswordError] = useState("")
 
   const handleLogin = async(e) =>{
     e.preventDefault()
 
     if(!validateEmail(email)){
-      setError("Please enter a valid email address")
+      setEmailError("Please enter a valid email address")
       return
     }
     if(!password){
-      setError("Please enter the password")
+      setPasswordError("Please enter the password")
       return
     }
 
-    setError("")
+    setPasswordError("")
+    setEmailError("")
     // LoginAPI
   }
   return (
     <div className='flex items-center justify-center mt-28 '>
       <div className='w-90 rounded bg-white px-7 py-10'>
         <form onSubmit={handleLogin}>
-          <h4 className='text-2xl mb-7 text-center'>Login</h4>
+          <h4 className='text-2xl mb-7 text-center'>Login</h4>  
           <input 
             type="text" 
             placeholder='Enter your email' 
             className='input-box' 
             value={email}
-            onChange={(e)=>setEmail(e.target.value)}
+            onChange={(e)=>{
+              setEmail(e.target.value)
+              setEmailError("")
+            }}
           />
+          {emailError && <p className='text-red-500 text-sm -mt-3 pb-3'>{emailError}</p>}
           <Password 
             password={password}
             setPassword={setPassword}
-            onChange={(e)=>setPassword(e.target.value)}
+            onChange={(e)=>{
+              setPassword(e.target.value)
+              setPasswordError("")
+            }}
           />
-          {error && <p className='text-red-500 text-sm pb-1'>{error}</p>}
+          {passwordError && <p className='text-red-500 text-sm pb-1'>{passwordError}</p>}
 
           <button type='submit' className='btn-primary mt-4 h-10 '>Login</button>
           <p className='text-sm text-center mt-4'>Not Registered Yet? {" "}<Link to={"/signup"} className='font-medium underline text-[#2B85FF]'>Create an account</Link></p>
