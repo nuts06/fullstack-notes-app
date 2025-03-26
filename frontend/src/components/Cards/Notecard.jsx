646be37f0fde724e475336a2f4f5f93d65814fba
Natasha <121818639+nuts06@users.noreply.github.com>
2025-03-26 16:08:26 +0530

    home ui begin

--- frontend/src/components/Cards/Notecard.jsx
+++ frontend/src/components/Cards/Notecard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { MdOutlinePushPin } from "react-icons/md"
 
-const Notecard = () => {
+const Notecard = ({isPinned, handlePinNote, content}) => {
   return (
     <div>
         <div className='flex items-center justify-between'>
@@ -11,7 +11,17 @@ const Notecard = () => {
             </div>
             <MdOutlinePushPin 
                 className={`icon-btn ${isPinned ? "text-[#2B85FF]" : "text-slate-300"}`}
+                onClick={handlePinNote}
             />
+        </div>
+        <p className='text-xs text-slate-600 mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus ex odit earum alias harum, nisi, iste eius dignissimos blanditiis qui in? Dolorem in id earum sequi veritatis doloribus sunt totam.
+        Velit commodi veniam quisquam itaque assumenda quam fuga sed vitae corrupti ad? Fugiat, esse error commodi iste laboriosam rerum, asperiores quo unde voluptatum, quae adipisci similique incidunt aliquid tempora vitae!
+        Recusandae 
+        </p>
+
+        {/* for tags */}
+        <div className='flex items-center justify-between mt-2'>
+          <div className='text-xs text-slate-500'>#tags</div>
         </div>
     </div>
   )
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Notecard from '../../components/Cards/Notecard'
 
 const Home = () => {
+  const [content, setContent] = useState("")
+  const handlePinNote = () =>[
+    alert("Pinned")
+  ]
   return (
     <div className='container mx-auto'>
       <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 max-md:m-5'>
-      <Notecard />
+      <Notecard 
+        handlePinNote={handlePinNote}
+      />
       <Notecard />
       <Notecard />
       <Notecard />
