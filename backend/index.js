d10742008bf3f3c29ae7e320e8430e68aea4789d
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-02 16:11:06 +0530

    connect the mongodb

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,18 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 
+
 // we have to config the dotenv before using it
 dotenv.config()
+
+
+// mongodb connection
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Mongo db connected")
+}).catch((err)=>{
+    console.log(err)
+})
+
 
 const app = express()
 
