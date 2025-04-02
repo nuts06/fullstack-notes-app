e8666d8e03516f9e948015b25ab70c9f9b47e3ad
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-02 19:19:59 +0530

    add middleware fot error handling

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 
 
-// we have to config the dotenv before using it
+// Loads environment variables from a .env file into process.env.
+// Needed to securely store sensitive data like database credentials (MONGO_URI).
 dotenv.config()
 
 
@@ -19,4 +20,19 @@ const app = express()
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000")
+})
+
+
+
+// global error handling middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+    
 })
