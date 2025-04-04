c59239f8573c0d15ae5b56a10f8839c102c61d17
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-04 12:36:33 +0530

    created the sign up api

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,13 +1,29 @@
 import User from "../models/user.model.js"
 import { errorHandler } from "../utils/error.js"
+import bcrypt from "bcryptjs"
 
-export const signup = async(req, res, next) =>{
-    const {username, userEmail, password} = req.body
+export const signup = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
 
-    const isValidUser = await User.findOne({email:userEmail})
+        // Check if user already exists
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "User already exists", success: false });
+        }
 
-    if(isValidUser){
-        // console.log("User already exists!")
-        return next(errorHandler(400, "User Already Exists"))
+        // Hash password and save user
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword,
+        });
+        console.log("New user to be saved:", newUser);
+
+        await newUser.save();
+        res.status(201).json({ message: "User Created Successfully", success: true });
+    } catch (err) {
+        res.status(500).json({ message: "Internal server error", success: false });
     }
-}
+};
