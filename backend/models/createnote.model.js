82f914eaed88fd520e4eec9a932580b02d5fa4b3
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-10 12:50:10 +0530

    create the modal for the notes

---
+++ backend/models/createnote.model.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const noteSchema = new mongoose.Schema({
+    title:{
+        type : String,
+        required : true
+    },
+    content:{
+        type : String,
+        required : true
+    },
+    tags:{
+        type : [String],
+        required : true
+    }
+})
+
+const Note = mongoose.model("Note", noteSchema)
+export default Note;
