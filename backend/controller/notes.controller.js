232d3136daae2eecbc0eaf6ad35f21a588061509
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-10 12:45:42 +0530

    create two api for fetch and delete the notes

---
+++ backend/controller/notes.controller.js
@@ -0,0 +1,50 @@
+import Note from "../models/createnote.model.js";
+
+export const createNote = async(req, res) => {
+    try{
+        const {title, content, tags} = req.body;
+        if (!title || !content || !tags) {
+            return res.status(400).json({ message: "Title, content and tags are required", success: false });
+        }
+         // Ensure tags is an array
+         if (!Array.isArray(tags)) {
+            return res.status(400).json({ message: "Tags must be an array", success: false });
+        }
+        const newNote = new Note({
+            title,
+            content,
+            tags
+        })
+        await newNote.save();
+        res.status(201).json({messgae:'Note Created Successfully', success: true, note: newNote})
+    } catch(err){
+        res.status(500).json({message: err.message, success: false})
+    }
+}
+
+export const getAllNotes = async(req, res) =>{
+    try{
+        const notes = await Note.find();
+        res.status(200).json({notes, success:true})
+    } catch(err){
+        res.status(500).json({message:err.message, success:false})
+    }
+}
+
+// DELETE: /api/notes/:id
+export const deleteNote = async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const deletedNote = await Note.findByIdAndDelete(id);
+  
+      if (!deletedNote) {
+        return res.status(404).json({ message: "Note not found", success: false });
+      }
+  
+      res.status(200).json({ message: "Note deleted successfully", success: true, note: deletedNote });
+    } catch (err) {
+      res.status(500).json({ message: err.message, success: false });
+    }
+  };
+  
