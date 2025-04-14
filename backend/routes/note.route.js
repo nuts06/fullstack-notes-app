6e41a26b5e99aa9461df69591ca5e38082f52775
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-14 18:15:03 +0530

    fix note route

--- backend/routes/note.route.js
+++ backend/routes/note.route.js
@@ -1,9 +1,11 @@
 import express from "express"
 import { createNote, getAllNotes, deleteNote } from "../controller/notes.controller.js"
+import { authMiddleware } from "../middlewares/authMiddleware.js"
+// import { authMiddleware } from "../middlewares/authMiddleware.js"
 
 const router = express.Router()
-router.post('/create-note', createNote)
-router.get('/get-all-notes', getAllNotes)
+router.post('/create-note', authMiddleware, createNote); 
+router.get('/get-all-notes', authMiddleware, getAllNotes);
 router.delete('/:id', deleteNote);
 
 
