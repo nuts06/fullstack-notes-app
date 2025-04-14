3e833aa9536690e325b7f02c7c705c1a888361ba
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-14 18:14:46 +0530

    fix routes in the backend

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from "express"
-import { signup } from "../controller/auth.controller.js"
+import { signup, verifyOtp } from "../controller/auth.controller.js"
 import { login } from "../controller/auth.controller.js"
+import { register } from "../controller/auth.controller.js"
 
 const router = express.Router()
 
 router.post("/signup", signup)
 router.post("/login", login)
+router.post('/send-otp', register)
+router.post('/verify-otp', verifyOtp)
 
 export default router
