8f1d097da51f0a4530dc9841b9afae6bfbb1bf16
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-15 16:42:54 +0530

    user modal changes

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,26 +1,45 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
     name: {
-        type:String,
-        required: true,
+        type: String,
+        required: false,
     },
     email: {
-        type:String,
+        type: String,
         required: true,
         unique: true,
     },
     password: {
-        type:String,
-        required: true,
+        type: String,
+        required: false,
+    },
+    contact: {
+        type: String,
+        required: false, // or you can set a validation pattern for contact (e.g., phone number)
+    },
+    country: {
+        type: String,
+        required: false,
+    },
+    state: {
+        type: String,
+        required: false,
+    },
+    city: {
+        type: String,
+        required: false,
+    },
+    postalcode: {
+        type: String,
+        required: false,
     },
     createdAt: {
-        type:Date,
-        default:Date.now
+        type: Date,
+        default: Date.now,
     }
-})
+});
 
+const User = mongoose.model("User", userSchema);
 
-const User = mongoose.model("User", userSchema)
-
-export default User
+export default User;
