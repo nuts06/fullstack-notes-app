29c6d9db297d64b791c6b8cf2a1c255f47ed5764
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-16 11:34:03 +0530

    fix the login page with the new theme

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -4,15 +4,17 @@ import {Link, useNavigate} from "react-router-dom"
 import { validateEmail } from '../../utils/helper'
 import axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify'
+import RegisterBackground from "../../assets/images/venusregister.jpg"
 
 const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  // const [error, setError] = useState("")
   const [emailError, setEmailError] = useState("")
   const [passwordError, setPasswordError] = useState("")
   const navigate = useNavigate()
 
+
+  // api calling for the handle login
   const handleLogin = async(e) =>{
     e.preventDefault()
 
@@ -32,7 +34,7 @@ const Login = () => {
       console.log("login response", response);
       if(response.data.success){
         localStorage.setItem("token", response.data.token)
-        
+
         toast.success("Login Successful", {
           position: 'top-right',
           autoClose:3000
@@ -41,15 +43,31 @@ const Login = () => {
         setPassword("")
         setTimeout(()=> navigate("/"), 3000)
       }
+      // console.log("response natasha", response.data.user.name)
+      localStorage.setItem("userName", response.data.user.name);
+      window.dispatchEvent(new Event("userLoggedIn"));
     })
     .catch(err =>{
       console.log("Login error", err);
-      if(err.response && err.response.status === 400)
-        alert(err.response.data.message)
+      if(err.response && err.response.status === 400){
+        // alert(err.response.data.message)
+      toast.error(err.response.data.message,{
+        position: 'top-right',
+        autoClose:2000
+      })}
       else if(err.response && err.response.status === 401)
-        alert("Invalid credentials")
-      else
-        alert("Something went wrong")
+      {
+        toast.error("Invalid credentials", {
+          position: 'top-right',
+          autoClose: 3000
+        });
+      }
+      else {
+        toast.error("Something went wrong", {
+          position: 'top-right',
+          autoClose: 3000
+        });
+      }
     })
     setEmail("")
     setPassword("")
@@ -58,39 +76,67 @@ const Login = () => {
     setEmailError("")
     // LoginAPI
   }
-  return (
-    <div className='flex items-center justify-center mt-28 '>
-      <div className='w-90 rounded bg-white px-7 py-10'>
-        <form onSubmit={handleLogin}>
-          <h4 className='text-2xl mb-7 text-center'>Login</h4>  
-          <input 
-            type="text" 
-            placeholder='Enter your email' 
-            className='input-box' 
-            value={email}
-            onChange={(e)=>{
-              setEmail(e.target.value)
-              setEmailError("")
-            }}
-          />
-          {emailError && <p className='text-red-500 text-sm -mt-3 pb-3'>{emailError}</p>}
-          <Password 
-            password={password}
-            setPassword={setPassword}
-            onChange={(e)=>{
-              setPassword(e.target.value)
-              setPasswordError("")
-            }}
-          />
-          {passwordError && <p className='text-red-500 text-sm pb-1'>{passwordError}</p>}
 
-          <button type='submit' disabled={!email || !password} className={`btn-primary mt-4 h-10 ${(!email || !password) ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}>Login</button>
-          <p className='text-sm text-center mt-4'>Not Registered Yet? {" "}<Link to={"/signup"} className='font-medium underline text-[#2B85FF]'>Create an account</Link></p>
-        </form>
+
+  return (
+    <div className="relative w-full h-screen">
+      {/* Background Image */}
+      <img 
+        className="fixed top-0 left-0 w-full h-full object-cover -z-10" 
+        src={RegisterBackground} 
+        alt="Background" 
+      />
+      {/* Form Container */}
+      <div className="flex justify-center items-center h-full px-4">
+      <form onSubmit={handleLogin} className="ml-4 sm:ml-10 md:ml-[5px] flex flex-col">
+      <h2 className="text-4xl font-semibold mb-6 text-center text-white mr-[35px]">Login</h2>
+      {/* <input 
+        type="text" 
+        placeholder='Enter your email' 
+        className="w-72 md:w-80 p-3 text-black border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 mb-2"  
+        value={email}
+        onChange={(e)=>{
+          setEmail(e.target.value)
+          setEmailError("")
+        }}
+      /> */}
+      <label className="block mb-2 text-white">Email</label>
+              <input 
+                type="text"
+                value={email}
+                onChange={(e)=>{
+                  setEmail(e.target.value)
+                  setEmailError("")
+                }}
+                className="w-[350px] text-white p-3 border border-gray-300 rounded mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                placeholder="Enter your email"
+                required 
+              />
+      {emailError && <p className='text-red-500 text-sm -mt-3 pb-3'>{emailError}</p>}
+  
+      <label className="block mb-2 text-white">Password</label>
+              <input 
+                type="password"
+                value={password}
+                onChange={(e)=>{
+                  setPassword(e.target.value)
+                  setPasswordError("")
+                }}
+                className="w-[350px] text-white p-3 border border-gray-300 rounded mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                placeholder="Enter your email"
+                required 
+              />
+              
+
+        {passwordError && <p className='text-red-500 text-sm pb-1'>{passwordError}</p>}
+
+        <button type='submit' disabled={!email || !password} className={`bg-orange-700 transition-all text-white mt-4 w-[350px] h-[48px]   ${(!email || !password) ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}>Login</button>
+
+        <p className='text-sm text-center mt-4 text-white'>Not Registered Yet? {" "}<Link to={"/register"} className='font-medium underline text-[#2B85FF]'>Create an account</Link></p>
+      </form>
       </div>
       <ToastContainer />
     </div>
-    
   )
 }
 
