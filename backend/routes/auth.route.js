4e2c2e5c43858953705a818687fd0ae6046d838f
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-16 11:30:50 +0530

    changes in auth routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { signup, verifyOtp, personalDetails } from "../controller/auth.controller.js"
+import { signup, verifyOtp, personalDetails, getPersonalDetails } from "../controller/auth.controller.js"
 import { login } from "../controller/auth.controller.js"
 import { register } from "../controller/auth.controller.js"
 
@@ -10,5 +10,6 @@ router.post("/login", login)
 router.post('/send-otp', register)
 router.post('/verify-otp', verifyOtp)
 router.post('/personal-details', personalDetails)
+router.post('/get-personal-details', getPersonalDetails)
 
 export default router
