7441c1c9f38fa2ef3be93a5cb602d029175c9de6
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-17 17:09:41 +0530

    auth routes added

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { signup, verifyOtp, personalDetails, getPersonalDetails } from "../controller/auth.controller.js"
+import { signup, verifyOtp, personalDetails, getPersonalDetails, resetPassword } from "../controller/auth.controller.js"
 import { login } from "../controller/auth.controller.js"
 import { register } from "../controller/auth.controller.js"
 
@@ -10,6 +10,7 @@ router.post("/login", login)
 router.post('/send-otp', register)
 router.post('/verify-otp', verifyOtp)
 router.post('/personal-details', personalDetails)
-router.post('/get-personal-details', getPersonalDetails)
+router.get('/get-personal-details/:userId', getPersonalDetails)
+router.post('/reset-password', resetPassword)
 
 export default router
