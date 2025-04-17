c35d3dcecc79fd045d51150ab6bd9fe02f9b904a
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-17 17:10:12 +0530

    changes in navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,11 +3,14 @@ import Searchbar from './Searchbar/Searchbar'
 import Profileinfo from './Cards/Profileinfo'
 import { useNavigate, Link } from 'react-router-dom';
 import { useEffect} from 'react';
+import axios from 'axios'
 
 const Navbar = () => {
   const [searchQuery, setSearchQuery] = useState("")
   // const [buttonText, setButtontext] = useState('Logout')
   const [name, setName] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
+  const [notes, setNotes] = useState([]);
 
   // useEffect(() => {
   //   const storedName = localStorage.getItem("userName");
@@ -37,9 +40,34 @@ const Navbar = () => {
   
 
   const navigate = useNavigate()
-  const handleSeacrh = ()=>{
-    alert("Searched")
+
+  // integrating search api
+  const handleSearch = async (query) => {
+    console.log("Search query:", query); // Log the query
+
+  if (!query || typeof query !== 'string') {
+    setIsSearching(false);
+    return;
   }
+
+  const trimmedQuery = query.trim();
+    try {
+      const token = localStorage.getItem("token"); // or wherever you store the JWT
+      const res = await axios.get("http://localhost:3000/api/notes/search", {
+        params: { q: trimmedQuery },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setNotes(res.data.notes);
+      setIsSearching(true); // show only search results
+  
+      console.log(res.data.notes); // do something with the search results
+    } catch (err) {
+      console.error("Search error:", err.response?.data || err.message);
+    }
+  };
 
   const onClearSearch = ()=>{
     setSearchQuery("")
@@ -66,14 +94,18 @@ const Navbar = () => {
       <Searchbar 
         value={searchQuery}
         onChange={({target})=> setSearchQuery(target.value)}
-        handleSeacrh={handleSeacrh}
+        handleSearch ={handleSearch }
         onClearSearch={onClearSearch}
       />
+
+      
       <Profileinfo 
         name={name}
         onLogout={onLogout}
         Buttontext={buttonText}
       />
+
+      
     </div>
   )
 }
